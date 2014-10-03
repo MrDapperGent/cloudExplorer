1e502515f47640c8cfb6905310797434a3b481bc
Phillip Tribble <phillip.tribble@gmail.com>
2014-10-02 19:26:53 -0700

    Release Notes

--- src/cloudExplorer/build.sh
+++ src/cloudExplorer/build.sh
@@ -31,6 +31,6 @@ chmod +x $LOCBUILD/cloudExplorer
 cd $LOC
 zip -r $ZIP $BUILD_NAME
 cd $LOCBUILD
-#java -jar $LOCBUILD/CloudExplorer.jar build $BUILD_NAME $ZIP $BUCKET
+java -jar $LOCBUILD/CloudExplorer.jar build $BUILD_NAME $ZIP $BUCKET
 echo;echo;echo ;ls $LOCBUILD;echo
 ./cloudExplorer
