0fe2b38bd72f2a77b8db45cd3c81b07983f7dc91
Phillip Tribble <phillip.tribble@gmail.com>
2014-10-05 00:01:05 -0700

    New API

--- src/cloudExplorer/ObjectProperties.java
+++ src/cloudExplorer/ObjectProperties.java
@@ -34,7 +34,7 @@ public class ObjectProperties implements Runnable {
 
                 if (mainFrame.object_item[i].isSelected()) {
                     jTextArea1.setText("\nFile Name: " + mainFrame.object_item[i].getText());
-                    jTextArea1.append("\nSize: " + mainFrame.bucket.getObjectInfo(mainFrame.object_item[i].getText(), mainFrame.cred.getAccess_key(), mainFrame.cred.getSecret_key(), mainFrame.bucket_item[mainFrame.active_bucket].getText(), mainFrame.cred.getEndpoint(), "objectsize") + " KB");
+                    jTextArea1.append("\nSize: " + mainFrame.bucket.getObjectInfo(mainFrame.object_item[i].getText(), mainFrame.cred.getAccess_key(), mainFrame.cred.getSecret_key(), mainFrame.bucket_item[mainFrame.active_bucket].getText(), mainFrame.cred.getEndpoint(), "objectsize") + " Bytes");
                     jTextArea1.append("\nModified Date: " + mainFrame.bucket.getObjectInfo(mainFrame.object_item[i].getText(), mainFrame.cred.getAccess_key(), mainFrame.cred.getSecret_key(), mainFrame.bucket_item[mainFrame.active_bucket].getText(), mainFrame.cred.getEndpoint(), "objectdate"));
                     break;
                 }
