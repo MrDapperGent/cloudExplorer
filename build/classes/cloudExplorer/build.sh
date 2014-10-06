be640981cb0d9cc63e992896c4adf1a0d66a8c8d
Phillip Tribble <phillip.tribble@gmail.com>
2014-10-06 08:42:22 -0700

    Adding build file

---
+++ build/classes/cloudExplorer/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+BUILD_NAME="cloudExplorer-3.5"
+SRC="$HOME/cloudExplorer"
+JAR="CloudExplorer.jar"
+JARHOME="$HOME/NetBeansProjects/CloudExplorer"
+JARLOC="$JARHOME/dist"
+README="$SRC/src/cloudExplorer/Release_Notes.txt"
+WINDOWS="$SRC/src/cloudExplorer/cloudExplorer"
+LINUX="$SRC/src/cloudExplorer/cloudExplorer.bat"
+LOC="$HOME"
+LOCBUILD="$LOC/$BUILD_NAME/"
+ZIP="$LOC/$BUILD_NAME.zip"
+BUCKET="cloudexplorer"
+
+rm -rf $LOC/$BUILD_NAME
+mkdir $LOC/$BUILD_NAME
+cd $SRC
+git pull
+cd $JARHOME
+ant
+rm -f $LOCBUILD/$JAR
+rm -f $ZIP
+rm -rf $LOCBUILD/home
+cp -rf $JARLOC/* $LOCBUILD
+cp -f $README $LOCBUILD
+cp -f $WINDOWS $LOCBUILD
+cp -f $LINUX $LOCBUILD
+chmod +x $LOCBUILD/cloudExplorer
+cd $LOC
+zip -r $ZIP $BUILD_NAME
+cd $LOCBUILD
+#java -jar $LOCBUILD/CloudExplorer.jar build $BUILD_NAME.zip $ZIP $BUCKET
+echo;echo;echo ;ls $LOCBUILD;echo
+./cloudExplorer
