ea8e2843c2a262bf2f923ddce89447b4ace91bde
Phillip Tribble <phillip.tribble@gmail.com>
2014-10-08 08:55:11 -0700

    Build script fix

--- src/cloudExplorer/BucketClass.java
+++ src/cloudExplorer/BucketClass.java
@@ -64,8 +64,6 @@ public class BucketClass {
 
     String makeBucket(String access_key, String secret_key, String bucket, String endpoint, String region) {
         String message = null;
-        System.out.print("\n" + bucket + " " + region + " " + access_key + " " + secret_key + " " + endpoint);
-
         AWSCredentials credentials = new BasicAWSCredentials(access_key, secret_key);
         AmazonS3 s3Client = new AmazonS3Client(credentials,
                 new ClientConfiguration().withSignerOverride("S3SignerType"));
