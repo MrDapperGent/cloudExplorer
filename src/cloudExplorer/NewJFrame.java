bb99a353fa5d0555f03588868146a0535c4a86af
Phillip Tribble <phillip.tribble@gmail.com>
2014-10-07 21:16:24 -0700

    GUI alignment

--- src/cloudExplorer/NewJFrame.java
+++ src/cloudExplorer/NewJFrame.java
@@ -1025,7 +1025,7 @@ public class NewJFrame extends javax.swing.JFrame implements ItemListener {
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jFileChooser2, javax.swing.GroupLayout.PREFERRED_SIZE, 874, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(jPanel4Layout.createSequentialGroup()
-                        .addGap(189, 189, 189)
+                        .addGap(175, 175, 175)
                         .addComponent(jButton16, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(0, 0, 0)
                         .addComponent(jToggleButton4)
