71054d0ed65019d6443fb84215070b08f4a69ab4
Phillip Tribble <phillip.tribble@gmail.com>
2015-03-02 17:51:42 -0800

    Graphing

--- src/cloudExplorer/Graph.java
+++ src/cloudExplorer/Graph.java
@@ -15,14 +15,31 @@
  */
 package cloudExplorer;
 
+import static cloudExplorer.PerformanceThread.latency_graph;
+import static cloudExplorer.PerformanceThread.ops_graph;
+import static cloudExplorer.PerformanceThread.throughput_graph;
+import com.googlecode.charts4j.AxisLabelsFactory;
+import com.googlecode.charts4j.Data;
+import com.googlecode.charts4j.DataUtil;
+import com.googlecode.charts4j.GCharts;
+import com.googlecode.charts4j.Plot;
+import com.googlecode.charts4j.Plots;
+import com.googlecode.charts4j.XYLineChart;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.net.URL;
+import java.util.Arrays;
+import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -33,6 +50,8 @@ public class Graph implements Runnable {
     String Home = System.getProperty("user.home");
     File config = new File(Home + File.separator + ".cloudExplorerIRC");
     String what = null;
+    double[] x;
+    double[] y;
 
     public Graph(NewJFrame Frame, String Awhat) {
         mainFrame = Frame;
@@ -48,10 +67,48 @@ public class Graph implements Runnable {
         }
     }
 
+    void graph(String title, String x_name, String y_name, String csv_file) {
+        try {
+
+            Data xdata = DataUtil.scaleWithinRange(0, x.length, x);
+            Data ydata = DataUtil.scaleWithinRange(0, y[1] * 4, y);
+            Plot plot = Plots.newXYLine(xdata, ydata);
+            plot.setColor(com.googlecode.charts4j.Color.BLUE);
+            XYLineChart xyLineChart = GCharts.newXYLineChart(plot);
+            xyLineChart.setSize(600, 300);
+            xyLineChart.setTitle(title);
+            xyLineChart.addXAxisLabels(AxisLabelsFactory.newAxisLabels(Arrays.asList("", x_name)));
+            xyLineChart.addYAxisLabels(AxisLabelsFactory.newAxisLabels(Arrays.asList("", y_name)));
+            xyLineChart.addXAxisLabels(AxisLabelsFactory.newNumericRangeAxisLabels(0, x.length + 1));
+            xyLineChart.addYAxisLabels(AxisLabelsFactory.newNumericRangeAxisLabels(0, y[1] * 4));
+            ImageIcon throughput_icon = (new ImageIcon(ImageIO.read(new URL(xyLineChart.toURLString()))));
+            JLabel label_throughput = new JLabel(throughput_icon);
+
+            //Configures the panel
+            NewJFrame.jPanel11.removeAll();
+            GridLayout layout = new GridLayout(0, 3);
+            NewJFrame.jPanel11.setLayout(layout);
+
+            NewJFrame.jPanel11.add(label_throughput);
+            try {
+                Image image_throughput = throughput_icon.getImage();
+                BufferedImage buffered_throughput_icon = (BufferedImage) image_throughput;
+                File outputfile = new File(Home + File.separator + "GRAPH-" + title + ".png");
+                ImageIO.write(buffered_throughput_icon, "png", outputfile);
+            } catch (Exception ex) {
+            }
+
+            NewJFrame.jPanel11.revalidate();
+            NewJFrame.jPanel11.repaint();
+            System.gc();
+        } catch (Exception graph) {
+        }
+    }
+
     public void run() {
 
         final JButton save = new JButton("Save");
-        final JButton graph = new JButton("Graph");
+        final JButton graph = new JButton("              Graph");
         final JButton close = new JButton("Close");
         final JLabel graph_name = new JLabel("Name:");
         final JLabel x_name = new JLabel("X-axis name:");
@@ -69,8 +126,8 @@ public class Graph implements Runnable {
         final JTextField x_whattograph_field = new JTextField("0");
         final JTextField y_whattograph_field = new JTextField("1");
         final JTextField graph_name_field = new JTextField("New Graph");
-        final JTextField x_name_field = new JTextField(null);
-        final JTextField y_name_field = new JTextField(null);
+        final JTextField x_name_field = new JTextField("X");
+        final JTextField y_name_field = new JTextField("Y");
         final JTextField x_graphsize_field = new JTextField("600");
         final JTextField y_graphsize_field = new JTextField("300");
 
@@ -132,7 +189,7 @@ public class Graph implements Runnable {
         mainFrame.jPanel11.setLayout(new BoxLayout(mainFrame.jPanel11, BoxLayout.Y_AXIS));
         mainFrame.jPanel11.add(graph_name);
         mainFrame.jPanel11.add(graph_name_field);
-          mainFrame.jPanel11.add(blank6);
+        mainFrame.jPanel11.add(blank6);
         mainFrame.jPanel11.add(graph_size_x);
         mainFrame.jPanel11.add(x_graphsize_field);
         mainFrame.jPanel11.add(graph_size_y);
