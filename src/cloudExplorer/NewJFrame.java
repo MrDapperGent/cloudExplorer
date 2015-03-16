0a1e63ee09924b39c630cea139704fb51ca649fd
Phillip Tribble <phillip.tribble@gmail.com>
2015-03-16 10:21:03 -0700

    Folders

--- src/cloudExplorer/NewJFrame.java
+++ src/cloudExplorer/NewJFrame.java
@@ -45,8 +45,8 @@ import javax.swing.plaf.ColorUIResource;
 
 public class NewJFrame extends javax.swing.JFrame implements ItemListener {
 
-    String major = "6";
-    String minor = "0";
+    String major = "5";
+    String minor = "3";
     String release_version = major + "." + minor;
     String version = "Cloud Explorer " + release_version;
     public boolean consoleToggle = false;
