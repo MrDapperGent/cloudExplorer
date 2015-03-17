ecd90db22252be2b0b6852d0f6a527f2eed398bc
Phillip Tribble <phillip.tribble@gmail.com>
2015-03-17 06:30:43 -0700

    Folders

--- src/cloudExplorer/MakeBucket.java
+++ src/cloudExplorer/MakeBucket.java
@@ -85,7 +85,7 @@ public class MakeBucket implements Runnable {
                     mainFrame.jPanel14.repaint();
                     mainFrame.jPanel14.revalidate();
                     mainFrame.jPanel14.validate();
-                    mainFrame.miniReload();
+                    mainFrame.reloadBuckets();
                 }
             });
 
