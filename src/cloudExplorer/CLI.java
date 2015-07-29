3250c608f10d8866199846386fb80883ac0da516
U-smorellato-PC\ptribble <phillip.tribble@gmail.com>
2015-07-29 15:21:11 -0700

    Fixes

--- src/cloudExplorer/CLI.java
+++ src/cloudExplorer/CLI.java
@@ -457,8 +457,9 @@ public class CLI {
 
                             if (folder != null) {
                                 if (object_array[i].contains(folder)) {
-                                    File dir = new File( destination + File.separator + cutit[0] + File.separator + transcoded_object);
-                                  //  String object = makeDirectory(destination + File.separator + cutit[0] + File.separator + transcoded_object);
+                                    File dir = new File(destination + File.separator + cutit[0] + File.separator + transcoded_object);
+                                    dir.mkdirs();
+//  String object = makeDirectory(destination + File.separator + cutit[0] + File.separator + transcoded_object);
                                     //String object = makeDirectory(destination + File.separator + cutit[0]);
                                     System.out.print("\nDebug making dir:" + destination + File.separator + cutit[0] + File.separator + transcoded_object);
                                     System.out.print("\nDebug dest file:" + destination + File.separator + cutit[0] + File.separator + transcoded_object + File.separator + cutit[cutit.length - 1]);
