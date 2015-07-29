e9ab1bb095e37dd8362292bd6686713042582cb6
U-smorellato-PC\ptribble <phillip.tribble@gmail.com>
2015-07-29 14:48:54 -0700

    Fixes

--- src/cloudExplorer/CLI.java
+++ src/cloudExplorer/CLI.java
@@ -457,9 +457,9 @@ public class CLI {
                             
                             if (folder != null) {
                                 if (object_array[i].contains(folder)) {
-                                    File dir = new File(destination + File.separator + cutit[0]);
+                                    File dir = new File(destination + File.separator + cutit[0] + File.separator + cutit[0]);
                                     System.out.print("\nDebug making dir:" + destination + File.separator + cutit[0]);
-                                    System.out.print("\nDebug dest file:" + destination + File.separator + cutit[0] + File.separator + cutit[1]);
+                                    System.out.print("\nDebug dest file:" + destination + File.separator + cutit[0] + File.separator + cutit[0]);
                                     dir.mkdirs();
                                     get = new Get(object_array[i], access_key, secret_key, bucket, endpoint, destination + File.separator + cutit[0] + File.separator + cutit[1], null);
                                     get.run();
