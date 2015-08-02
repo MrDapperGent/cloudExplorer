0dad617881c012defa38a46cff04f789f71c324b
Phillip Tribble <phillip.tribble@gmail.com>
2015-08-02 13:33:19 -0700

    Snapshots

--- src/cloudExplorer/build.sh
+++ src/cloudExplorer/build.sh
@@ -2,8 +2,6 @@
 BUILD_NAME="cloudExplorer-6.04"
 SRC="$HOME/cloudExplorer"
 README="$SRC/src/cloudExplorer/Release_Notes.txt"
-WINDOWS="$SRC/src/cloudExplorer/cloudExplorer"
-LINUX="$SRC/src/cloudExplorer/cloudExplorer.bat"
 ZIP="$SRC/$BUILD_NAME.zip"
 
 cd $SRC
@@ -11,8 +9,6 @@ ant
 rm -rf $SRC/$BUILD_NAME
 cp -rf $SRC/dist $BUILD_NAME
 cp -f $README $SRC/$BUILD_NAME
-cp -f $WINDOWS $SRC/$BUILD_NAME
-cp -f $LINUX $SRC/$BUILD_NAME
 cp -rf $SRC/libs $SRC/$BUILD_NAME/
 rm -f *.zip
 zip --exclude=*.git* -r $ZIP $BUILD_NAME
