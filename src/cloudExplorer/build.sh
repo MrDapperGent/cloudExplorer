c149e843104f4aad1b2f7dfd540481260a16ad17
Phillip Tribble <philliptribble@Phillips-MacBook-Pro.local>
2015-10-14 16:05:54 -0700

    New Libraries

--- src/cloudExplorer/build.sh
+++ src/cloudExplorer/build.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 BUILD_NAME="cloudExplorer-7.0"
 SRC="$HOME/cloudExplorer"
-README="$SRC/src/cloudExplorer/Release_Notes.txt"
 ZIP="$SRC/$BUILD_NAME.zip"
 
 cd $SRC
 ant
 rm -rf $SRC/$BUILD_NAME
 cp -rf $SRC/dist $BUILD_NAME
-cp -f $README $SRC/$BUILD_NAME
-cp -rf $SRC/libs $SRC/$BUILD_NAME/
 rm -f *.zip
 zip --exclude=*.git* -r $ZIP $BUILD_NAME
 java -jar $SRC/$BUILD_NAME/CloudExplorer.jar help
