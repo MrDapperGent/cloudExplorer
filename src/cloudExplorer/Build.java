bdabc07ef5966a58d90bc03411c2ae58950e4535
Phillip Tribble <phillip.tribble@gmail.com>
2016-10-12 12:12:58 -0700

    Envar fix for build

--- src/cloudExplorer/Build.java
+++ src/cloudExplorer/Build.java
@@ -137,10 +137,10 @@ public class Build {
             System.out.print("\n\nUploading " + build_file.getAbsolutePath().toString() + "........");
             put = new Put(build_file.getAbsolutePath().toString(), access_key, secret_key, bucket, endpoint, build_name, false, false, false);
             put.startc(build_file.getAbsolutePath().toString(), access_key, secret_key, bucket, endpoint, build_name, false, false, false);
-            objectacl.setACLpublic(build_name, access_key, secret_key, endpoint, bucket);
-            String url = objectacl.setACLurl(build_name, access_key, secret_key, endpoint, bucket);
-            url = url.replace("Pre-Signed URL = ", "");
-            System.out.print("\n\n" + url);
+          //  objectacl.setACLpublic(build_name, access_key, secret_key, endpoint, bucket);
+           // String url = objectacl.setACLurl(build_name, access_key, secret_key, endpoint, bucket);
+            //url = url.replace("Pre-Signed URL = ", "");
+           // System.out.print("\n\n" + url);
 
         } catch (Exception send) {
             System.out.print("\n\nAn Error has occured while uploading the file.\n\n");
