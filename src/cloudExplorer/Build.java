507290c010ad2dee4c6f9ebbc0068adb0707e39e
Phillip Tribble <phillip.tribble@gmail.com>
2016-10-12 13:13:33 -0700

    Envar fix for build

--- src/cloudExplorer/Build.java
+++ src/cloudExplorer/Build.java
@@ -138,7 +138,7 @@ public class Build {
             put = new Put(build_file.getAbsolutePath().toString(), access_key, secret_key, bucket, endpoint, build_name, false, false, false);
             put.startc(build_file.getAbsolutePath().toString(), access_key, secret_key, bucket, endpoint, build_name, false, false, false);
             objectacl.setACLpublic(build_name, access_key, secret_key, endpoint, bucket);
-            String url = objectacl.setACLurl(build_name, access_key, secret_key, endpoint, bucket);
+            //String url = objectacl.setACLurl(build_name, access_key, secret_key, endpoint, bucket);
             //url = url.replace("Pre-Signed URL = ", "");
             // System.out.print("\n\n" + url);
 
