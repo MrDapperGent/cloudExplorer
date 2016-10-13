f2eec2d90af0b3a8177cc7bce8d67e6f7222b0e6
Phillip Tribble <phillip.tribble@gmail.com>
2016-10-13 15:45:52 -0700

    New Upgrade Beta

--- src/cloudExplorer/Update.java
+++ src/cloudExplorer/Update.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -14,6 +15,8 @@ import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 
@@ -85,9 +88,14 @@ public class Update
                     }
                 }
                 message("\nFile copy complete. Deleteing old upgrade directory.");
-                scan_location.delete();
+                try {
+                    FileUtils.deleteDirectory(new File(scan_location.toString()));
+                    check_zip.delete();
+                } catch (IOException ex) {
+                    Logger.getLogger(Update.class.getName()).log(Level.SEVERE, null, ex);
+                }
                 message("\nUpgrade complete. Please restart Cloud Explorer.");
-//      NewJFrame.jMenuItem23.doClick();
+                //      NewJFrame.jMenuItem23.doClick();
             } else {
                 message("\nError: Zip file not found!");
             }
