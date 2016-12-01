9b8b9a04925eab5464f2fac87fb1fe5b57e57bc0
Phillip Tribble <phillip.tribble@gmail.com>
2016-12-01 09:24:34 -0800

    GUI FIX

--- src/cloudExplorer/NewJFrame.java
+++ src/cloudExplorer/NewJFrame.java
@@ -689,7 +689,7 @@ public class NewJFrame extends javax.swing.JFrame implements ItemListener {
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
-                .addGap(62, 62, 62)
+                .addGap(10, 10, 10)
                 .addComponent(jPanel12, javax.swing.GroupLayout.PREFERRED_SIZE, 440, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(47, 47, 47)
                 .addComponent(jPanel17, javax.swing.GroupLayout.PREFERRED_SIZE, 528, javax.swing.GroupLayout.PREFERRED_SIZE)
